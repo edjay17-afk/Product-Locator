4dd3a70ab4226a733273604bf90774b84d94115c
edjay17-afk <edjaymar17@gmail.com>
2026-08-17 10:56:44 +0800

    Fix Netlify function routing: pass full /api/* path to serverless Express handler

--- functions/api.js
+++ functions/api.js
@@ -1,4 +1,7 @@
 const serverless = require('serverless-http');
 const app = require('../server');
 
+// Wrap the Express app as a Netlify serverless function.
+// The /api/* -> /.netlify/functions/api/:splat redirect in netlify.toml
+// routes all API calls here automatically.
 module.exports.handler = serverless(app);
